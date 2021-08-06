bf43b3d5c803a32dea87b55abf91067568869b6b
Bernardo Sampaio <bernardosfmachado@hotmail.com>
2021-08-05 22:15:58 -0300

    removing unused files

--- config/database.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose')
-module.exports = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/forentec', {useNewUrlParser: true, useUnifiedTopology: true})
-
-mongoose.Error.messages.general.required = 'O campo "{PATH}" é obrigatório'
